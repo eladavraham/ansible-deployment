6c381d50ce61aa480ae9d4a9162f19c8c976e058
Jaroslav Libak <jaroslav.libak@hp.com>
2015-09-17 13:36:04 +0200

    * added more playbooks for gaia service management * solved problem with duplication of services in files and parametric services. Services are now defined in all.yaml

--- filter_plugins/custom_filters.py
+++ filter_plugins/custom_filters.py
@@ -8,10 +8,26 @@ def ec2_instance_info(value, return_key):
 
     return results
 
+def has_key(value, key):
+    return value.has_key(key)
+
+def list_of_dicts_to_list(value, first_key, second_key=None):
+    # creates new list that will contain values of first_key if found, if not found then second_key
+    results = []
+    for mydict in value:
+      if mydict.has_key(first_key):
+          results.append(mydict[first_key])
+      elif second_key and mydict.has_key(second_key):
+          results.append(mydict[second_key])
+
+    return results
+
 class FilterModule(object):
     ''' Ansible core jinja2 filters '''
 
     def filters(self):
         return {
             'ec2_instance_info': ec2_instance_info,
+            'has_key': has_key,
+            'list_of_dicts_to_list': list_of_dicts_to_list
         }
