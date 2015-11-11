ece16dc0e9fbef71002b45cbaccaf5fd9a7dc615
Jaroslav Libak <jaroslav.libak@hp.com>
2015-11-11 17:03:26 +0100

    added parameter for gaia_fleet_dir

--- main.sh
+++ main.sh
@@ -4,11 +4,13 @@ export EC2_INI_PATH=inventory/ec2.ini
 # read input parameters
 vflag=""
 provisioner="ec2"
+gaia_fleet_dir=".gaia-fleet"
 while [ $# -gt 0 ]
 do
   case "$1" in
     -v) vflag="-vvvv";;
     -f) yamlfile="$2"; shift;;
+    -g) gaia_fleet_dir="$2"; shift;;
     -e) env="$2"; shift;;
     -s) service="$2"; shift;;
     -p) provisioner="$2"; shift;;
@@ -20,4 +22,4 @@ do
   shift
 done
 
-ansible-playbook --extra-vars "env=$env provisioner=$provisioner service=$service" $yamlfile $vflag
+ansible-playbook --extra-vars "env=$env provisioner=$provisioner gaia_fleet_dir=$gaia_fleet_dir service=$service" $yamlfile $vflag
